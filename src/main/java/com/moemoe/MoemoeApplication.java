627fbcbd92e4b40e222a89def92098af7780ae62
seunghun_park2 <seunghun_park2@tmax.co.kr>
2024-10-29 23:32:43 +0900

    init commit

---
+++ src/main/java/com/moemoe/MoemoeApplication.java
@@ -0,0 +1,13 @@
+package com.moemoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoemoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoemoeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moemoe/MoemoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moemoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoemoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

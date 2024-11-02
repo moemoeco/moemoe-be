63682619393f6fb94729625ed71901e032b64cef
Phodol2 <psh19930326@gmail.com>
2024-11-02 11:22:17 +0900

    feat : Kakao URL Builder 추가 및 인가 URL 생성 로직

---
+++ src/main/java/com/moemoe/service/KakaoURLBuilder.java
@@ -0,0 +1,38 @@
+package com.moemoe.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@Component
+public class KakaoURLBuilder {
+    @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
+    private String authorizationUri;
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String tokenUri;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String clientSecret;
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String redirectUri;
+    @Value("${spring.security.oauth2.client.registration.kakao.scope}")
+    private String scopes;
+
+
+    public String authorize() {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("client_id", List.of(clientId));
+        params.put("redirect_uri", List.of(redirectUri));
+        params.put("scope", List.of(scopes));
+
+        return UriComponentsBuilder.fromHttpUrl(authorizationUri)
+                .queryParams(params)
+                .build()
+                .toString();
+    }
+}
---
+++ src/test/java/com/moemoe/service/KakaoURLBuilderTest.java
@@ -0,0 +1,25 @@
+package com.moemoe.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class KakaoURLBuilderTest {
+    @Autowired
+    private KakaoURLBuilder kakaoURLBuilder;
+
+    @Test
+    void authorize() {
+        String expectedUrl = "https://kauth.kakao.com/oauth/authorize?client_id=client-id&redirect_uri=redirect-uri&scope=profile_nickname,name";
+        String actualUrl = kakaoURLBuilder.authorize();
+
+        assertThat(actualUrl)
+                .isEqualTo(expectedUrl);
+    }
+}

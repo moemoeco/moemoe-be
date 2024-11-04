174493f870ac9fe05f64add0caacdf62bd932538
Phodol2 <psh19930326@gmail.com>
2024-11-04 22:00:24 +0900

    fix : User Domain 내 필드 추가 및 인덱스 자동 생성 설정

--- src/main/java/com/moemoe/domain/User.java
+++ src/main/java/com/moemoe/domain/User.java
@@ -1,19 +1,45 @@
 package com.moemoe.domain;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
-@Document(collection = "USERS")
+@Document(collection = "users")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class User {
     @Id
     private String id;
+    private long socialId;
     private String name;
+    @Indexed(unique = true)
+    private String email;
+    private String gender;
+    private String birthyear;
+    private String birthday;
+    private String thumbnailImageUrl;
+    private String profileImageUrl;
 
     @Builder
-    public User(String name) {
+    public User(long socialId,
+                String name,
+                String email,
+                String gender,
+                String birthyear,
+                String birthday,
+                String thumbnailImageUrl,
+                String profileImageUrl) {
+        this.socialId = socialId;
         this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.birthyear = birthyear;
+        this.birthday = birthday;
+        this.thumbnailImageUrl = thumbnailImageUrl;
+        this.profileImageUrl = profileImageUrl;
     }
 }

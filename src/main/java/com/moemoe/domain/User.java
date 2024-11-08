d60b3a0ad2f9c27999db5872520e727cfd9bd096
Phodol2 <psh19930326@gmail.com>
2024-11-08 11:51:31 +0900

    fix : User Entity에 Role 추가 및 UserDetails 상속
    - Spring Security 에서 사용자의 정보를 담기 위한 인터페이스 추가

--- src/main/java/com/moemoe/domain/User.java
+++ src/main/java/com/moemoe/domain/User.java
@@ -7,13 +7,20 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Document(collection = "users")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class User {
+public class User implements UserDetails {
     @Id
     private String id;
+    private UserRole role;
     private long socialId;
     private String name;
     @Indexed(unique = true)
@@ -27,6 +34,7 @@ public class User {
     @Builder
     public User(long socialId,
                 String name,
+                UserRole role,
                 String email,
                 String gender,
                 String birthyear,
@@ -36,10 +44,28 @@ public class User {
         this.socialId = socialId;
         this.name = name;
         this.email = email;
+        this.role = role;
         this.gender = gender;
         this.birthyear = birthyear;
         this.birthday = birthday;
         this.thumbnailImageUrl = thumbnailImageUrl;
         this.profileImageUrl = profileImageUrl;
+    }
+
+
+    // Spring Security UserDetails Area
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getPassword() {
+        return String.valueOf(socialId);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 }

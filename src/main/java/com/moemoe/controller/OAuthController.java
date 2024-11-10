1002c5f07127a36ad64a013605f4769507ae37ca
Phodol2 <psh19930326@gmail.com>
2024-11-10 19:54:30 +0900

    feat : 카카오 소셜 로그인 서비스 추가
    - 소셜 로그인 시 조회된 데이터를 바탕으로 회원 데이터 생성

--- src/main/java/com/moemoe/controller/OAuthController.java
+++ src/main/java/com/moemoe/controller/OAuthController.java
@@ -1,20 +1,30 @@
 package com.moemoe.controller;
 
 
+import com.moemoe.dto.LoginTokenResponse;
 import com.moemoe.service.KakaoOAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RestController("/oauth")
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/oauth")
+@CrossOrigin(origins = "*", methods = RequestMethod.GET)
 public class OAuthController {
     private final KakaoOAuthService kakaoOAuthService;
 
     @GetMapping("/login-page")
     public String loginPage() {
+        log.info("Login page");
         return kakaoOAuthService.authorize();
+    }
+
+    @GetMapping("/login")
+    public LoginTokenResponse login(
+            @RequestParam("code") String code) {
+        log.info("Login");
+        return kakaoOAuthService.login(code);
     }
 }
--- src/main/java/com/moemoe/service/KakaoOAuthService.java
+++ src/main/java/com/moemoe/service/KakaoOAuthService.java
@@ -1,16 +1,84 @@
 package com.moemoe.service;
 
+import com.moemoe.domain.RefreshToken;
+import com.moemoe.domain.User;
+import com.moemoe.domain.UserRole;
+import com.moemoe.dto.KakaoTokenResponse;
+import com.moemoe.dto.KakaoUserInfoResponse;
+import com.moemoe.dto.LoginTokenResponse;
+import com.moemoe.http.KakaoTokenClient;
+import com.moemoe.http.KakaoUserInfoClient;
+import com.moemoe.repository.RefreshTokenEntityRepository;
+import com.moemoe.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class KakaoOAuthService {
     private final KakaoURLBuilder kakaoURLBuilder;
+    private final KakaoTokenClient kakaoTokenClient;
+    private final KakaoUserInfoClient kakaoUserInfoClient;
+    private final UserEntityRepository userEntityRepository;
+    private final RefreshTokenEntityRepository refreshTokenEntityRepository;
+    private final JwtService jwtService;
 
     public String authorize() {
         return kakaoURLBuilder.authorize();
+    }
+
+    @Transactional
+    public LoginTokenResponse login(String code) {
+        KakaoTokenResponse token = getToken(code);
+        KakaoUserInfoResponse userInfo = getKakaoUserInfoResponse(token);
+        User userEntity = getUserEntity(userInfo);
+
+        Map<String, String> claims = new HashMap<>();
+        claims.put("email", userEntity.getEmail());
+        claims.put("role", userEntity.getRole().name());
+
+        final String accessToken = jwtService.createAccessToken(claims, userEntity);
+        final String refreshToken = jwtService.createRefreshToken(claims, userEntity);
+
+        refreshTokenEntityRepository.save(RefreshToken.of(userEntity.getEmail(), refreshToken));
+        return LoginTokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    private User getUserEntity(KakaoUserInfoResponse userInfo) {
+        KakaoUserInfoResponse.KakaoAccount kakaoAccount = userInfo.kakaoAccount();
+        KakaoUserInfoResponse.Profile profile = kakaoAccount.profile();
+        return userEntityRepository.findByEmail(kakaoAccount.email())
+                .orElseGet(() -> userEntityRepository.save(User.builder()
+                        .socialId(userInfo.id())
+                        .email(kakaoAccount.email())
+                        .name(kakaoAccount.name())
+                        .role(UserRole.USER)
+                        .gender(kakaoAccount.gender())
+                        .birthday(kakaoAccount.birthday())
+                        .birthyear(kakaoAccount.birthyear())
+                        .profileImageUrl(profile.profileImageUrl())
+                        .thumbnailImageUrl(profile.thumbnailImageUrl())
+                        .build()));
+    }
+
+    private KakaoUserInfoResponse getKakaoUserInfoResponse(KakaoTokenResponse token) {
+        String userInfoUrl = kakaoURLBuilder.getUserInfo();
+        return kakaoUserInfoClient.getUserInfo(URI.create(userInfoUrl), token.getAuthorizationToken());
+    }
+
+    private KakaoTokenResponse getToken(String code) {
+        String tokenUrl = kakaoURLBuilder.getToken(code);
+        return kakaoTokenClient.getToken(URI.create(tokenUrl));
     }
 }

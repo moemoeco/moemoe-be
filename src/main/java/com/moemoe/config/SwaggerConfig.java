b138517200c60d801e6760e5ba2725949ae8cc96
Phodol2 <psh19930326@gmail.com>
2024-11-10 19:56:37 +0900

    feat : swagger 추가

---
+++ src/main/java/com/moemoe/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.moemoe.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        Info info = new Info()
+                .version("1.0")
+                .title("MoeMoe")
+                .description("MoeMoe Application API");
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
+                .info(info);
+    }
+}

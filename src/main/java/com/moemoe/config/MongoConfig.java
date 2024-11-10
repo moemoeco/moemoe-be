a83908f481f32c2c8d21ab0e556d49df3e4baac6
Phodol2 <psh19930326@gmail.com>
2024-11-10 19:56:27 +0900

    feat : MongoDB 데이터에 _class 필드를 추가하지 않도록 configuration 추가

---
+++ src/main/java/com/moemoe/config/MongoConfig.java
@@ -0,0 +1,26 @@
+package com.moemoe.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+@RequiredArgsConstructor
+public class MongoConfig {
+    private final MongoDatabaseFactory mongoDatabaseFactory;
+    private final MongoMappingContext mongoMappingContext;
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        DefaultDbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+}

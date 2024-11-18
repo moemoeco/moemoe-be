0e14a9926ff0706d7ac3b355810760b5d453eaad
Phodol2 <psh19930326@gmail.com>
2024-11-18 21:44:11 +0900

    refactor : OAuth Service에 Template 메서드 패턴 적용

--- src/main/java/com/moemoe/service/KakaoOAuthService.java
+++ src/main/java/com/moemoe/service/KakaoOAuthService.java
@@ -1,63 +1,47 @@
 package com.moemoe.service;
 
-import com.moemoe.domain.RefreshToken;
 import com.moemoe.domain.User;
 import com.moemoe.domain.UserRole;
-import com.moemoe.dto.kakao.KakaoUserInfoResponse;
-import com.moemoe.dto.LoginTokenResponse;
 import com.moemoe.dto.TokenResponse;
 import com.moemoe.dto.UserInfoResponse;
+import com.moemoe.dto.kakao.KakaoUserInfoResponse;
+import com.moemoe.http.builder.KakaoUrlBuilder;
 import com.moemoe.http.builder.UrlBuilder;
 import com.moemoe.http.client.kakao.KakaoTokenClient;
 import com.moemoe.http.client.kakao.KakaoUserInfoClient;
 import com.moemoe.repository.RefreshTokenEntityRepository;
 import com.moemoe.repository.UserEntityRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class KakaoOAuthService {
+public class KakaoOAuthService extends OAuthTemplate {
     private final UrlBuilder kakaoUrlBuilder;
     private final KakaoTokenClient kakaoTokenClient;
     private final KakaoUserInfoClient kakaoUserInfoClient;
-    private final UserEntityRepository userEntityRepository;
-    private final RefreshTokenEntityRepository refreshTokenEntityRepository;
-    private final JwtService jwtService;
 
+    protected KakaoOAuthService(RefreshTokenEntityRepository refreshTokenEntityRepository,
+                                UserEntityRepository userEntityRepository,
+                                JwtService jwtService,
+                                KakaoUrlBuilder kakaoUrlBuilder,
+                                KakaoTokenClient kakaoTokenClient,
+                                KakaoUserInfoClient kakaoUserInfoClient) {
+        super(userEntityRepository, refreshTokenEntityRepository, jwtService);
+        this.kakaoUrlBuilder = kakaoUrlBuilder;
+        this.kakaoTokenClient = kakaoTokenClient;
+        this.kakaoUserInfoClient = kakaoUserInfoClient;
+    }
+
+    @Override
     public String authorize(String state) {
         return kakaoUrlBuilder.getAuthorizeUrl(state);
     }
 
-    @Transactional
-    public LoginTokenResponse login(String code, String state) {
-        TokenResponse token = getToken(code, state);
-        UserInfoResponse userInfo = getUserInfo(token);
-        User userEntity = getUserEntity(userInfo);
-
-        Map<String, String> claims = new HashMap<>();
-        claims.put("email", userEntity.getEmail());
-        claims.put("role", userEntity.getRole().name());
-
-        final String accessToken = jwtService.createAccessToken(claims, userEntity);
-        final String refreshToken = jwtService.createRefreshToken(claims, userEntity);
-
-        refreshTokenEntityRepository.save(RefreshToken.of(userEntity.getEmail(), refreshToken));
-        return LoginTokenResponse.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
-
-    private User getUserEntity(UserInfoResponse userInfo) {
+    @Override
+    protected User getUserEntity(UserInfoResponse userInfo) {
         KakaoUserInfoResponse kakaoUserInfo = (KakaoUserInfoResponse) userInfo;
         KakaoUserInfoResponse.KakaoAccount kakaoAccount = kakaoUserInfo.kakaoAccount();
         KakaoUserInfoResponse.Profile profile = kakaoAccount.profile();
@@ -74,12 +58,14 @@ public class KakaoOAuthService {
                         .build()));
     }
 
-    private UserInfoResponse getUserInfo(TokenResponse token) {
+    @Override
+    protected UserInfoResponse getUserInfo(TokenResponse token) {
         String userInfoUrl = kakaoUrlBuilder.getUserInfoUrl();
         return kakaoUserInfoClient.getUserInfo(URI.create(userInfoUrl), token.authorizationToken());
     }
 
-    private TokenResponse getToken(String code, String state) {
+    @Override
+    protected TokenResponse getToken(String code, String state) {
         String tokenUrl = kakaoUrlBuilder.getTokenUrl(code, state);
         return kakaoTokenClient.getToken(URI.create(tokenUrl));
     }
--- src/main/java/com/moemoe/service/NaverOAuthService.java
+++ src/main/java/com/moemoe/service/NaverOAuthService.java
@@ -1,61 +1,47 @@
 package com.moemoe.service;
 
-import com.moemoe.domain.RefreshToken;
 import com.moemoe.domain.User;
 import com.moemoe.domain.UserRole;
-import com.moemoe.dto.*;
+import com.moemoe.dto.TokenResponse;
+import com.moemoe.dto.UserInfoResponse;
 import com.moemoe.dto.naver.NaverUserInfoResponse;
+import com.moemoe.http.builder.NaverUrlBuilder;
 import com.moemoe.http.builder.UrlBuilder;
 import com.moemoe.http.client.naver.NaverTokenClient;
 import com.moemoe.http.client.naver.NaverUserInfoClient;
 import com.moemoe.repository.RefreshTokenEntityRepository;
 import com.moemoe.repository.UserEntityRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class NaverOAuthService {
+public class NaverOAuthService extends OAuthTemplate {
     private final UrlBuilder naverUrlBuilder;
     private final NaverTokenClient naverTokenClient;
     private final NaverUserInfoClient naverUserInfoClient;
-    private final UserEntityRepository userEntityRepository;
-    private final RefreshTokenEntityRepository refreshTokenEntityRepository;
-    private final JwtService jwtService;
 
+    public NaverOAuthService(RefreshTokenEntityRepository refreshTokenEntityRepository,
+                             UserEntityRepository userEntityRepository,
+                             JwtService jwtService,
+                             NaverUrlBuilder naverUrlBuilder,
+                             NaverTokenClient naverTokenClient,
+                             NaverUserInfoClient naverUserInfoClient) {
+        super(userEntityRepository, refreshTokenEntityRepository, jwtService);
+        this.naverUrlBuilder = naverUrlBuilder;
+        this.naverTokenClient = naverTokenClient;
+        this.naverUserInfoClient = naverUserInfoClient;
+    }
+
+    @Override
     public String authorize(String state) {
         return naverUrlBuilder.getAuthorizeUrl(state);
     }
 
-    @Transactional
-    public LoginTokenResponse login(String code, String state) {
-        TokenResponse token = getToken(code, state);
-        UserInfoResponse userInfo = getUserInfo(token);
-        User userEntity = getUserEntity(userInfo);
-
-        Map<String, String> claims = new HashMap<>();
-        claims.put("email", userEntity.getEmail());
-        claims.put("role", userEntity.getRole().name());
-
-        final String accessToken = jwtService.createAccessToken(claims, userEntity);
-        final String refreshToken = jwtService.createRefreshToken(claims, userEntity);
-
-        refreshTokenEntityRepository.save(RefreshToken.of(userEntity.getEmail(), refreshToken));
-        return LoginTokenResponse.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
-
-    private User getUserEntity(UserInfoResponse userInfo) {
+    @Override
+    protected User getUserEntity(UserInfoResponse userInfo) {
         NaverUserInfoResponse naverUserInfo = (NaverUserInfoResponse) userInfo;
         NaverUserInfoResponse.NaverAccount naverAccount = naverUserInfo.naverAccount();
         return userEntityRepository.findByEmail(naverAccount.email())
@@ -71,12 +57,14 @@ public class NaverOAuthService {
                         .build()));
     }
 
-    private UserInfoResponse getUserInfo(TokenResponse token) {
+    @Override
+    protected UserInfoResponse getUserInfo(TokenResponse token) {
         String userInfoUrl = naverUrlBuilder.getUserInfoUrl();
         return naverUserInfoClient.getUserInfo(URI.create(userInfoUrl), token.authorizationToken());
     }
 
-    private TokenResponse getToken(String code, String state) {
+    @Override
+    protected TokenResponse getToken(String code, String state) {
         String tokenUrl = naverUrlBuilder.getTokenUrl(code, state);
         return naverTokenClient.getToken(URI.create(tokenUrl));
     }

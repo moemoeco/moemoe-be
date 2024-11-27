3cf49f6640c11a06cc69f5b0277adb3ce2c17024
Phodol2 <psh19930326@gmail.com>
2024-11-27 20:46:50 +0900

    fix : Kakao User Info Response 내 불필요한 필드 제거

--- module-client/src/main/java/com/moemoe/client/http/dto/kakao/KakaoUserInfoResponse.java
+++ module-client/src/main/java/com/moemoe/client/http/dto/kakao/KakaoUserInfoResponse.java
@@ -11,22 +11,17 @@ public record KakaoUserInfoResponse(
     public record KakaoAccount(
             Profile profile,
             String name,
-            @JsonProperty("is_email_valid") boolean isEmailValid,
-            @JsonProperty("is_email_verified") boolean isEmailVerified,
             String email,
             String birthyear,
             String birthday,
             @JsonProperty("birthday_type") String birthdayType,
             String gender
     ) {
-    }
 
-    public record Profile(
-            String nickname,
-            @JsonProperty("thumbnail_image_url") String thumbnailImageUrl,
-            @JsonProperty("profile_image_url") String profileImageUrl,
-            @JsonProperty("is_default_image") boolean isDefaultImage,
-            @JsonProperty("is_default_nickname") boolean isDefaultNickname
-    ) {
+        public record Profile(
+                String nickname,
+                @JsonProperty("profile_image_url") String profileImageUrl
+        ) {
+        }
     }
 }

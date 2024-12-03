cde96d1af402541248a372ed97992db3c21e0718
Phodol2 <psh19930326@gmail.com>
2024-12-03 21:53:34 +0900

    fix : 기존 단일 모듈 파일 제거

--- src/main/java/com/moemoe/http/builder/UrlBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.moemoe.http.builder;
-
-public interface UrlBuilder {
-    String getAuthorizeUrl(String state);
-
-    String getTokenUrl(String code, String state);
-
-    String getUserInfoUrl();
-}

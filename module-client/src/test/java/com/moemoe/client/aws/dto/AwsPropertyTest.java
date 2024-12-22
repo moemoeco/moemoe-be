8097794e3cbc994c3966d51e8d4332523bbbc331
Phodol2 <psh19930326@gmail.com>
2024-12-22 13:46:59 +0900

    feat : AWS Property 를 바인딩하기 위한 클래스 생성

---
+++ module-client/src/test/java/com/moemoe/client/aws/dto/AwsPropertyTest.java
@@ -0,0 +1,35 @@
+package com.moemoe.client.aws.dto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import software.amazon.awssdk.regions.Region;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = {
+        "cloud.aws.accessKey=testAccessKey",
+        "cloud.aws.secretKey=testSecretKey",
+        "cloud.aws.region=ap-northeast-2"
+})
+@EnableConfigurationProperties(AwsProperty.class)
+class AwsPropertyTest {
+    @Autowired
+    private AwsProperty awsProperty;
+
+    @Test
+    void propertyBinding() {
+        // Then
+        assertThat(awsProperty.getAccessKey())
+                .isEqualTo("testAccessKey");
+        assertThat(awsProperty.getSecretKey())
+                .isEqualTo("testSecretKey");
+        assertThat(awsProperty.getRegion())
+                .isEqualTo(Region.AP_NORTHEAST_2);
+    }
+
+}

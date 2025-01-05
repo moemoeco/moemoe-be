6dc4cb75f6f53439e60b404653ff8cba24e0a55a
Phodol2 <psh19930326@gmail.com>
2025-01-05 17:14:03 +0900

    fix : 상품 등록 API 에 @Valid 어노테이션 추가

--- module-api/src/main/java/com/moemoe/api/controller/ProductController.java
+++ module-api/src/main/java/com/moemoe/api/controller/ProductController.java
@@ -3,11 +3,15 @@ package com.moemoe.api.controller;
 
 import com.moemoe.core.request.RegisterProductRequest;
 import com.moemoe.core.service.ProductService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -21,8 +25,8 @@ public class ProductController {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String register(
-            @RequestPart(value = "request") RegisterProductRequest request,
-            @Size(min = 1, max = 10, message = "Images must include at least 1 item and up to 10 items.") @RequestPart(value = "imageList") List<MultipartFile> imageList
+            @Valid @RequestPart(value = "request") RegisterProductRequest request,
+            @Valid @Size(min = 1, max = 10, message = "Images must include at least 1 item and up to 10 items.") @RequestPart(value = "imageList") List<MultipartFile> imageList
     ) {
         return productService.register(request, imageList);
     }

3c4e8fe925f952508c45aa063623d59c0ef422b7
Phodol2 <psh19930326@gmail.com>
2025-01-12 14:40:49 +0900

    test : Cursor 기반 페이지네이션 쿼리 테스트 추가

--- module-domain-mongo/src/test/java/com/moemoe/mongo/config/MongoTestConfig.java
+++ module-domain-mongo/src/test/java/com/moemoe/mongo/config/MongoTestConfig.java
@@ -1,9 +1,11 @@
 package com.moemoe.mongo.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @TestConfiguration
 @EnableMongoRepositories(basePackages = "com.moemoe.mongo")
+@ComponentScan(basePackages = "com.moemoe.mongo")
 public class MongoTestConfig {
 }

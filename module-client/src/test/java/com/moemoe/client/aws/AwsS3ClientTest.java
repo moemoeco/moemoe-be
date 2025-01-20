dafb9bf8982be3c78e085d691ebf2b8060a91ce5
Phodol2 <psh19930326@gmail.com>
2025-01-20 22:26:36 +0900

    feat : aws s3 버킷에 저장된 이미지의 presigned url을 조회하는 서비스 구현

--- module-client/src/test/java/com/moemoe/client/aws/AwsS3ClientTest.java
+++ module-client/src/test/java/com/moemoe/client/aws/AwsS3ClientTest.java
@@ -2,25 +2,31 @@ package com.moemoe.client.aws;
 
 import com.moemoe.client.aws.dto.AwsProperty;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.http.SdkHttpResponse;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -36,6 +42,7 @@ class AwsS3ClientTest {
     private AwsProperty awsProperty;
     private static final String BUCKET_NAME = "test";
     private static S3Client s3Client;
+    private static S3Presigner s3Presigner;
 
     @Container
     static LocalStackContainer container = new LocalStackContainer()
@@ -48,6 +55,7 @@ class AwsS3ClientTest {
                 container.getSecretKey()
         );
         Region region = Region.of(container.getRegion());
+        // create s3 client
         s3Client = S3Client.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
                 .endpointOverride(container.getEndpoint())
@@ -58,7 +66,24 @@ class AwsS3ClientTest {
                 .build();
         s3Client.createBucket(createBucketRequest);
 
+        // create s3 presigner
+        s3Presigner = S3Presigner.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .endpointOverride(container.getEndpoint())
+                .region(region)
+                .build();
+    }
 
+    @BeforeEach
+    void setUp() {
+        given(awsProperty.getAccessKey())
+                .willReturn(container.getAccessKey());
+        given(awsProperty.getSecretKey())
+                .willReturn(container.getSecretKey());
+        given(awsProperty.getRegion())
+                .willReturn(Region.of(container.getRegion()));
+        given(awsProperty.getBucketName())
+                .willReturn(BUCKET_NAME);
     }
 
     @AfterEach
@@ -99,15 +124,6 @@ class AwsS3ClientTest {
     @Test
     void upload() throws IOException {
         // given
-        given(awsProperty.getAccessKey())
-                .willReturn(container.getAccessKey());
-        given(awsProperty.getSecretKey())
-                .willReturn(container.getSecretKey());
-        given(awsProperty.getRegion())
-                .willReturn(Region.of(container.getRegion()));
-        given(awsProperty.getBucketName())
-                .willReturn(BUCKET_NAME);
-
         String fileName = "image.jpg";
         byte[] fileContent = "test content".getBytes();
         MockMultipartFile mockMultipartFile = new MockMultipartFile(
@@ -134,5 +150,63 @@ class AwsS3ClientTest {
                 .isNotNull();
         assertThat(headObjectResponse.contentLength())
                 .isEqualTo(fileContent.length);
+    }
+
+    @Test
+    void getPreSignedUrl() throws IOException {
+        // given
+        String fileName = "image.jpg";
+        byte[] fileContent = "test content".getBytes();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "file",
+                fileName,
+                IMAGE_JPEG_VALUE,
+                new ByteArrayInputStream(fileContent)
+        );
+
+        String s3ObjectKey = "uploads/" + fileName;
+        uploadMultipartFile(s3ObjectKey, mockMultipartFile);
+
+        // when
+        String preSignedUrl = awsS3Client.getPreSignedUrl(s3Presigner, s3ObjectKey);
+
+        // then
+        assertThat(preSignedUrl)
+                .isNotEmpty()
+                .contains(List.of(s3ObjectKey));
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(preSignedUrl).openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        assertThat(responseCode)
+                .isEqualTo(HttpURLConnection.HTTP_OK);
+        String downloadedContent = new String(connection.getInputStream().readAllBytes());
+        assertThat(downloadedContent)
+                .isEqualTo("test content");
+    }
+
+    private void uploadMultipartFile(String s3ObjectKey, MultipartFile multipartFile) {
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(awsProperty.getBucketName())
+                    .key(s3ObjectKey)
+                    .contentType(multipartFile.getContentType())
+                    .build();
+
+            PutObjectResponse putObjectResponse = s3Client.putObject(
+                    putObjectRequest,
+                    RequestBody.fromInputStream(inputStream, multipartFile.getSize())
+            );
+
+            SdkHttpResponse sdkHttpResponse = putObjectResponse.sdkHttpResponse();
+            if (sdkHttpResponse.isSuccessful()) {
+                log.info("S3 upload succeeded: S3 Object Key: {} , Status Code : {}", s3ObjectKey, sdkHttpResponse.statusCode());
+            } else {
+                throw new IllegalArgumentException("S3 upload failed: Status code : " + sdkHttpResponse.statusCode());
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("An error occurred during file upload: " + e.getMessage(), e);
+        }
     }
 }

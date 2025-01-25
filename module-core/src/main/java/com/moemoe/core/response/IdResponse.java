bc620b2283939ef0bde3a76d9c375a957084cbde
Phodol2 <psh19930326@gmail.com>
2025-01-25 20:34:11 +0900

    fix : Response와 Request에 equals, hasCode 재정의

--- module-core/src/main/java/com/moemoe/core/response/IdResponse.java
+++ module-core/src/main/java/com/moemoe/core/response/IdResponse.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
+import java.util.Objects;
+
 @Getter
 @NoArgsConstructor
 public class IdResponse {
@@ -11,5 +13,17 @@ public class IdResponse {
 
     public IdResponse(ObjectId objectId) {
         this.id = objectId.toHexString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        IdResponse response = (IdResponse) o;
+        return Objects.equals(getId(), response.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 }

dbb991006137026feb3f0cf41f61d5be20bde096
Phodol2 <psh19930326@gmail.com>
2025-02-02 12:12:25 +0900

    fix : API Controller Test Template 내 invoke 메서드 추가
    - mockMvc를 이용해 request를 실행
    - hasAuth를 이용해 Autentication Header 추가

--- module-api/src/test/java/com/moemoe/api/AbstractControllerTest.java
+++ module-api/src/test/java/com/moemoe/api/AbstractControllerTest.java
@@ -2,6 +2,7 @@ package com.moemoe.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moemoe.api.config.web.SecurityConfig;
 import com.moemoe.core.service.UserService;
 import com.moemoe.core.service.jwt.JwtService;
 import lombok.extern.slf4j.Slf4j;
@@ -9,23 +10,33 @@ import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.moemoe.core.service.jwt.JwtService.AUTHENTICATION_HEADER;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 @Slf4j
 @Disabled
 @AutoConfigureMockMvc
 @WithMockUser(username = "user@moemoe.com", roles = "USER")
+@Import(SecurityConfig.class)
 public abstract class AbstractControllerTest {
     @MockBean
     protected JwtService jwtService;
     @MockBean
-    private UserService userService;
+    protected UserService userService;
     @Autowired
     protected MockMvc mockMvc;
     protected final ObjectMapper objectMapper = new ObjectMapper();
@@ -53,10 +64,42 @@ public abstract class AbstractControllerTest {
         }
     }
 
+    protected MvcResult invoke(MockHttpServletRequestBuilder builder, ResultMatcher resultMatcher, boolean hasAuth) {
+        if (hasAuth) {
+
+            String accessToken = "expectedAccessToken";
+            given(jwtService.isValidToken(accessToken, "user@moemoe.com"))
+                    .willReturn(true);
+            given(jwtService.getEmail(accessToken))
+                    .willReturn("user@moemoe.com");
+            given(jwtService.getRole(accessToken))
+                    .willReturn("USER");
+            builder = builder.header(AUTHENTICATION_HEADER, "Bearer " + accessToken);
+        }
+        try {
+            return mockMvc.perform(builder)
+                    .andExpect(resultMatcher)
+                    .andDo(print())
+                    .andReturn();
+        } catch (Exception e) {
+            fail("api invocation failed.", e);
+            throw new IllegalStateException("api invocation failed.");
+        }
+    }
+
     protected String convertRequestToJson(Object request) {
         try {
             return objectMapper.writeValueAsString(request);
         } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected <T> T convertResponseToClass(MvcResult mvcResult, Class<T> type) {
+        try {
+            String content = mvcResult.getResponse().getContentAsString();
+            return objectMapper.readValue(content, type);
+        } catch (JsonProcessingException | UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }

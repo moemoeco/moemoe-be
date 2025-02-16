f0c57118da6b43c19dc4ad6c94e048be0e41300a
Phodol2 <psh19930326@gmail.com>
2025-02-16 17:41:31 +0900

    feat : S3ObjectKey를 이용해 특정 Bucket에 존재하는 Object 삭제 메서드 추가
    - S3ObjectKeyList를 이용해 모든 S3Object 삭제
    - 테스트 코드 추가 및 삭제 검증

--- module-client/src/main/java/com/moemoe/client/aws/AwsS3Client.java
+++ module-client/src/main/java/com/moemoe/client/aws/AwsS3Client.java
@@ -1,17 +1,18 @@
 package com.moemoe.client.aws;
 
 import com.moemoe.client.aws.dto.AwsProperty;
+import com.moemoe.client.exception.ClientRuntimeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.SdkHttpResponse;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
@@ -19,6 +20,7 @@ import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequ
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.Duration;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -59,6 +61,27 @@ public class AwsS3Client {
             }
         } catch (IOException e) {
             throw new IllegalArgumentException("An error occurred during file upload: " + e.getMessage(), e);
+        }
+    }
+
+    public void delete(S3Client s3Client, List<String> s3ObjectKeyList) {
+        if (ObjectUtils.isEmpty(s3ObjectKeyList)) {
+            throw new IllegalArgumentException("삭제할 객체 키 목록이 비어 있습니다.");
+        }
+
+        DeleteObjectsRequest deleteObjectsRequest = DeleteObjectsRequest.builder()
+                .bucket(awsProperty.getBucketName())
+                .delete(builder -> builder.objects(s3ObjectKeyList.stream()
+                        .map(key -> ObjectIdentifier.builder().key(key).build())
+                        .toList()))
+                .build();
+
+        try {
+            DeleteObjectsResponse response = s3Client.deleteObjects(deleteObjectsRequest);
+            log.info("삭제된 객체: {}", response.deleted());
+        } catch (S3Exception e) {
+            log.error("S3 객체 삭제 중 오류 발생: {}", e.awsErrorDetails().errorMessage());
+            throw new ClientRuntimeException(e.getMessage());
         }
     }
 

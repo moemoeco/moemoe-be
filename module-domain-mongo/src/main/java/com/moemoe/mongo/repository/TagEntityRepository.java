89e01bdb745a8913cf62b065c9253246b49a7406
Phodol2 <psh19930326@gmail.com>
2025-04-22 00:21:34 +0900

    feat : Update 쿼리를 MongoTemplate를 이용하여 업데이트 하도록 변경
    - @Update 어노테이션을 이용한 업데이트의 경우 @LastModifiedDate 를 이용한 Auditing이 적용되지 않음
    - MongoTemplate를 이용해 Update Query를 실행할 때 updateAt을 수동으로 업데이트하도록 변경
      - MongoUpdateQueryUtil 을 이용하여 공통화

--- module-domain-mongo/src/main/java/com/moemoe/mongo/repository/TagEntityRepository.java
+++ module-domain-mongo/src/main/java/com/moemoe/mongo/repository/TagEntityRepository.java
@@ -2,28 +2,19 @@ package com.moemoe.mongo.repository;
 
 
 import com.moemoe.mongo.entity.TagEntity;
+import com.moemoe.mongo.repository.custom.TagEntityUpdateRepository;
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.Update;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface TagEntityRepository extends MongoRepository<TagEntity, ObjectId> {
+public interface TagEntityRepository extends MongoRepository<TagEntity, ObjectId>, TagEntityUpdateRepository {
     Optional<TagEntity> findTagEntityByName(String name);
 
     List<TagEntity> findAllByNameIn(Collection<String> names);
-
-    @Query("{ 'name': ?0 }")
-    @Update("{ '$inc': { 'productsCount': 1 } }")
-    void incrementProductsCount(String name);
-
-    @Query("{ 'name': ?0 }")
-    @Update("{ '$inc': { 'productsCount': -1 } }")
-    void decrementProductsCount(String name);
 
     List<TagEntity> findTop20ByNameStartingWith(String prefix, Sort sort);
 }

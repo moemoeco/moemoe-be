4a2983c3c91f3e774799457c7af75f85a47a4f34
Phodol2 <psh19930326@gmail.com>
2025-05-07 13:57:23 +0900

    feat : Chat Module 생성
    - 실시간 채팅 서비스를 위한 모듈 생성

---
+++ module-chat/src/main/java/com/moemoe/chat/ModuleChatApplication.java
@@ -0,0 +1,13 @@
+package com.moemoe.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleChatApplication.class, args);
+    }
+
+}
---
+++ module-chat/src/test/java/com/moemoe/chat/ModuleChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moemoe.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

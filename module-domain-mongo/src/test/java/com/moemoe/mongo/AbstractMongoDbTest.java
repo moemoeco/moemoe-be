91a39c8435a5aeb637facdd5a227040afe60753b
Phodol2 <psh19930326@gmail.com>
2025-05-13 10:28:03 +0900

    test : MongoTemplate 클래스를 상위 클래스로 이관
    - 모든 테스트 클래스에서 사용하기 위해 MongoTemplate를 AbstractMongoDbTest 클래스로 이관

--- module-domain-mongo/src/test/java/com/moemoe/mongo/AbstractMongoDbTest.java
+++ module-domain-mongo/src/test/java/com/moemoe/mongo/AbstractMongoDbTest.java
@@ -2,10 +2,14 @@ package com.moemoe.mongo;
 
 import com.moemoe.mongo.config.MongoConfig;
 import com.moemoe.mongo.config.MongoTestConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 
 @DataMongoTest
 @ContextConfiguration(classes = {MongoConfig.class, MongoTestConfig.class})
 public abstract class AbstractMongoDbTest {
+    @Autowired
+    protected MongoTemplate mongoTemplate;
 }

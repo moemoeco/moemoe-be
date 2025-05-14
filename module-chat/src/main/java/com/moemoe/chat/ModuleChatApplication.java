c5578c31c1fab31e068eee20c362d92ded25f338
Phodol2 <psh19930326@gmail.com>
2025-05-14 17:22:53 +0900

    feat: STOMP 이벤트 리스너 추가
    - subscribe 이벤트 수신 시 Redis에 구독자 인덱스 추가
    - unsubscribe 이벤트 수신 시 Redis에서 구독자 인덱스 제거
    - disconnect 이벤트 발생 시 해당 유저의 모든 구독 인덱스 정리
    - Listener의 Redis Repository를 사용하기 위해 basePackage 추가
    - 각 케이스 별 테스트 추

--- module-chat/src/main/java/com/moemoe/chat/ModuleChatApplication.java
+++ module-chat/src/main/java/com/moemoe/chat/ModuleChatApplication.java
@@ -3,7 +3,7 @@ package com.moemoe.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.moemoe.chat", "com.moemoe.mongo"})
+@SpringBootApplication(scanBasePackages = {"com.moemoe.chat", "com.moemoe.mongo", "com.moemoe.redis"})
 public class ModuleChatApplication {
 
     public static void main(String[] args) {

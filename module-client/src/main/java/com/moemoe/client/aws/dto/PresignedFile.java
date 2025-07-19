c33cbfaf54fb7b0780eb8dd59f5843d4460bccac
Phodol2 <psh19930326@gmail.com>
2025-07-19 14:22:14 +0900

    fix : PresignedFile의 응답 값에 file key를 입력 받도록 변경

--- module-client/src/main/java/com/moemoe/client/aws/dto/PresignedFile.java
+++ module-client/src/main/java/com/moemoe/client/aws/dto/PresignedFile.java
@@ -3,6 +3,9 @@ package com.moemoe.client.aws.dto;
 public record PresignedFile(
         String fileName,
         String uploadUrl,
-        String fileUrl
+        String fileKey
 ) {
+    public static PresignedFile of(String fileName, String uploadUrl, String fileKey) {
+        return new PresignedFile(fileName, uploadUrl, fileKey);
+    }
 }

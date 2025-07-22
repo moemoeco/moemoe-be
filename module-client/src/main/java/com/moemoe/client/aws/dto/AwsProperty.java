3151a970a8852c31163016fffd38af0997b75a50
Phodol2 <psh19930326@gmail.com>
2025-07-22 12:28:32 +0900

    fix : AwsProperty prefix 수정
    - aws를 기반으로 s3, credentials 계층으로 prefix 수정

--- module-client/src/main/java/com/moemoe/client/aws/dto/AwsProperty.java
+++ module-client/src/main/java/com/moemoe/client/aws/dto/AwsProperty.java
@@ -1,18 +1,53 @@
 package com.moemoe.client.aws.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 import software.amazon.awssdk.regions.Region;
 
 @Getter
-@Setter
-@Component
-@ConfigurationProperties(prefix = "cloud.aws")
+@ConfigurationProperties(prefix = "aws")
 public class AwsProperty {
-    private String accessKey;
-    private String secretKey;
-    private String bucketName;
-    private Region region;
+    private final Credentials credentials;
+    private final Region region;
+    private final S3 s3;
+
+    public AwsProperty(Credentials credentials, Region region, S3 s3) {
+        if (s3 == null) {
+            throw new IllegalArgumentException("AWS S3 configuration is required");
+        }
+        if (credentials == null) {
+            throw new IllegalArgumentException("AWS credentials configuration is required");
+        }
+
+        this.credentials = credentials;
+        this.region = region;
+        this.s3 = s3;
+    }
+
+    public String getBucketName() {
+        return s3.getBucketName();
+    }
+
+    public String getAccessKey() {
+        return credentials.getAccessKey();
+    }
+
+    public String getSecretKey() {
+        return credentials.getSecretKey();
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class S3 {
+        private final String bucketName;
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Credentials {
+        private final String accessKey;
+        private final String secretKey;
+    }
 }
